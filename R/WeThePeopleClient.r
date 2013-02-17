b3642b65f9d379c2170ce5e9da5b4acaaac14bcf
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-02-17 16:11:36 -0500

    First run at loading all petitions up to a user-defined limit. client will paginate behind the scenes to load all of the data at once.

--- R/WeThePeopleClient.r
+++ R/WeThePeopleClient.r
@@ -14,18 +14,64 @@ WeThePeopleClient <- function(key='') {
     sprintf("%s/%ss.json", BASE_URL, resource)
   }
 
+  #' Retrieves all petitions up to the given limit.
+  get_petitions <- function(limit=NA) {
+
+    result <- NULL
+    count <- 0
+
+    repeat {
+
+      fully_qualified_url <- we_the_people_url("petition")
+      params <- list(key=key, limit=100, offset=count)
+
+      message("Getting petitions from the We The People API. URL: ", fully_qualified_url, " PARAMS: ", toJSON(params))
+
+      petitions_raw <- fromJSON(getForm(fully_qualified_url, .params=params))
+      result_count <- length(petitions_raw$results)
+
+      message("Loaded ", result_count, " petitions")
+
+      if(result_count == 0) {
+        break
+      }
+
+      petitions.df <- petitions_raw_to_data_frame(petitions_raw)
+
+      if(is.null(result)) {
+        result <- petitions.df
+      }
+      else {
+        result <- merge(result, petitions.df, all=TRUE)
+      }
+
+      count <- nrow(result)
+
+      if(!is.na(limit) && count >= limit) {
+        break
+      }
+
+    }
+
+    if(!is.na(limit)) {
+      result <- head(result, n=limit)
+    }
+
+    result
+
+  }
+
   #' Loads petitions from the API or from a flat JSON file.
   #' @param file optional file to load from instead of hitting the API
-  petitions <- function(file=NA) {
+  #' @param limit to limit the number of petitions returned
+  petitions <- function(file=NA, limit=NA) {
 
     if(is.na(file)) {
-      petitions_raw <- fromJSON(getForm(we_the_people_url("petition"), .params=list(key=key)))
+      get_petitions(limit=limit)
     }
     else {
-      petitions_raw <- fromJSON(file=file)
+      petitions_raw_to_data_frame(fromJSON(file=file))
     }
-
-    petitions_raw_to_data_frame(petitions_raw)
 
   }
 
