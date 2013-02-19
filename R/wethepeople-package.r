7fdb445ca176436dadf2b9ffdb3d54e87a4b02b1
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-02-19 00:06:39 -0500

    Add petition data to the package, instead of loading petitions from a file. Fix examples.

--- R/wethepeople-package.r
+++ R/wethepeople-package.r
@@ -6,10 +6,20 @@
 #' @author Yoni Ben-Meshulam \email{yoni.bmesh@@gmail.com}
 #' @examples
 #' # Load a sample set of petitions and generate some plots:
-#' petitions <- sample_petitions()
+#' data(petitions)
 #' plot_issues_over_time(petitions)
-#' plot_wordcloud(petitions)
+#' plot_wordcloud(petitions, 'title')
 #'
 #' # Create an API Client
 #' client <- WeThePeopleClient('SOME_API_KEY')
+NULL
+
+#' Petitions loaded Feb 19, 2013
+#'
+#' Sample petitions loaded from the API on February 19, 2013.
+#'
+#' @name petitions
+#' @docType data
+#' @author Yoni Ben-Meshulam \email{yoni@@opower.com}
+#' @keywords data
 NULL
