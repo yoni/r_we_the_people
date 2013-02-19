5f0a23414948189d91b8d8939cd789a7d05cf30f
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-02-18 23:59:44 -0500

    Add a function for loading signatures for a given petition. Genericize POSIXct field creation. Genericize resource URL creation.

--- R/WeThePeopleClient.r
+++ R/WeThePeopleClient.r
@@ -8,10 +8,22 @@
 #' client <- WeThePeopleClient('MY_API_KEY')
 WeThePeopleClient <- function(key='') {
 
-  BASE_URL <- "https://petitions.whitehouse.gov/api/v1/"
+  BASE_URL <- "https://petitions.whitehouse.gov/api/v1"
 
-  we_the_people_url <- function(resource) {
-    sprintf("%s/%ss.json", BASE_URL, resource)
+  #' Constructs a resource URL based on parent-child relationships defined in the API.
+  we_the_people_url <- function(resource, parent=NA, parent_id=NA) {
+
+    has_nested_resource_params <- is.na(c(parent, parent_id))
+    if(any(has_nested_resource_params) && !all(has_nested_resource_params))
+      stop("You must provide both a parent and an id if using a nested resource.")
+
+    if(is.na(parent)) {
+      sprintf("%s/%ss.json", BASE_URL, resource)
+    }
+    else {
+      sprintf("%s/%ss/%s/%ss.json", BASE_URL, parent, parent_id, resource)
+    }
+
   }
 
   #' Retrieves all petitions up to the given limit.
@@ -36,7 +48,7 @@ WeThePeopleClient <- function(key='') {
         break
       }
 
-      petitions.df <- petitions_raw_to_data_frame(petitions_raw)
+      petitions.df <- entity_list_to_data_frame(petitions_raw)
 
       if(is.null(result)) {
         result <- petitions.df
@@ -61,6 +73,49 @@ WeThePeopleClient <- function(key='') {
 
   }
 
+  #' Retrieves signatures for the given petition_id.
+  #' Due to a bug, signatures will not be available until the next release.
+  #' See https://github.com/WhiteHouse/hackathon/issues/6
+  #' https://petitions.whitehouse.gov/api/v1/petitions/1234/signatures.json?key=bCYUnMF3782k9s8&mock=1
+  signatures <- function(petition_id, mock=1) {
+
+    # Override petition_id to be an integer for now. See the bug for details on why.
+    petition_id <- 1234
+
+    signatures_vector <- c()
+    count <- 0
+
+    repeat {
+
+      fully_qualified_url <- we_the_people_url('signature', parent='petition', parent_id=petition_id)
+      params <- list(key=key, limit=100, offset=count, mock=mock)
+
+      message("Getting signatures from the We The People API. URL: ", fully_qualified_url, " PARAMS: ", toJSON(params))
+
+      signatures_raw <- fromJSON(getForm(fully_qualified_url, .params=params))
+      signatures_vector_count <- length(signatures_raw$results)
+
+      message("Loaded ", signatures_vector_count, " signatures")
+
+      signatures_vector <- c(signatures_vector, signatures_raw$results)
+
+      count <- nrow(signatures_vector)
+
+      if(signatures_vector_count == 0 || mock == 1) {
+        break
+      }
+
+    }
+
+    signatures_df <- ldply(signatures_vector, data.frame, stringsAsFactors=FALSE)
+    signatures_df$petition_id <- petition_id
+
+    signatures_df <- add_datetime_fields(signatures_df, c('created'))
+
+    signatures_df
+
+  }
+
   #' Loads petitions from the API or from a flat JSON file.
   #' @param file optional file to load from instead of hitting the API
   #' @param limit to limit the number of petitions returned
@@ -70,13 +125,14 @@ WeThePeopleClient <- function(key='') {
       get_petitions(limit=limit)
     }
     else {
-      petitions_raw_to_data_frame(fromJSON(file=file))
+      entity_list_to_data_frame(fromJSON(file=file))
     }
 
   }
 
   interface <- list(
-    petitions=petitions
+    petitions=petitions,
+    signatures=signatures
   )
 
   class(interface) <- 'WeThePeople'
@@ -89,20 +145,28 @@ WeThePeopleClient <- function(key='') {
 #' @param file the path to the JSON file, assumed to be in the format of a We The People API result
 #' @return petitions a data frame of the petitions
 #' @importFrom rjson fromJSON
-petitions_raw_to_data_frame <- function(petitions_raw) {
+entity_list_to_data_frame <- function(entity_list) {
 
-  petitions <- ldply(
-    petitions_raw$results,
+  entities <- ldply(
+    entity_list$results,
     function(item) {
       as.data.frame(unlist(item, recursive=FALSE), stringsAsFactors=FALSE)
     }
   )
 
-  # Convert to POSIXct fields for convenience.
-  for(field in c('created', 'deadline')) {
-    petitions[[sprintf('%s_POSIXct', field)]] <- as.POSIXct(petitions[[field]], origin="1970-01-01")
+  entities <- add_datetime_fields(entities, c('created', 'deadline'))
+
+  entities
+
+}
+
+add_datetime_fields <- function(entities, fields) {
+
+  for(field in fields) {
+    if(field %in% names(entities))
+      entities[[sprintf('%s_POSIXct', field)]] <- as.POSIXct(as.numeric(entities[[field]]), origin="1970-01-01")
   }
 
-  petitions
+  entities
 
 }
