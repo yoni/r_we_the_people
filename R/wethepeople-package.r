1ac14c793080f8a092156a6170e5140bba172b09
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-03-03 12:37:52 -0500

    Appease the CRAN maintainers by adding reference to so-called globalVariables.

--- R/wethepeople-package.r
+++ R/wethepeople-package.r
@@ -34,3 +34,7 @@ NULL
 #' @author Yoni Ben-Meshulam \email{yoni@@opower.com}
 #' @keywords data
 NULL
+
+# Appease CRAN by removing all "no visible binding for global variable" notes.
+# See http://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+if(getRversion() >= "2.15.1") globalVariables(c("created_POSIXct", "deadline_POSIXct", "issue", "signature.count", "status"))
